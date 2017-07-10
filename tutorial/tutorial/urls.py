67146e6572c6b557f92bae859ae5ad52db8ec644
Jason BouKheir <jason.boukheir@gmail.com>
2017-07-10 14:22:26 -0500

    Django REST framework tutorial part 7: Schemas & Client libraries

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import url, include
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
 ]
